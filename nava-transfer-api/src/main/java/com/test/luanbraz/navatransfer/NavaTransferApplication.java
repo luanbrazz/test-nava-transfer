3e070d6b004f41bae27d33931423b005c487ab1b
Luan Braz <luan_silvacpv@hotmail.com>
2024-12-06 09:52:26 -0300

    feat: started spring project and added initial application.properties for database

---
+++ nava-transfer-api/src/main/java/com/test/luanbraz/navatransfer/NavaTransferApplication.java
@@ -0,0 +1,13 @@
+package com.test.luanbraz.navatransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavaTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavaTransferApplication.class, args);
+	}
+
+}
---
+++ nava-transfer-api/src/test/java/com/test/luanbraz/navatransfer/NavaTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.luanbraz.navatransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavaTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
